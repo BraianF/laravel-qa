a05c4f67471103d7335ce212dfa817836e859cae
BraianF <braianantoniolli@gmail.com>
2020-02-08 17:30:09 -0300

    Generated fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -14,9 +14,20 @@ class Answer extends Model
 	public function user()
 	{
 		return $this->belongsTo(User::class);
-    }
+	}
 
-    public function getBodyHtmlAttribute(){
+	public function getBodyHtmlAttribute(){
 		return \Parsedown::instance()->text($this->body);
+	}
+
+	public static function boot()
+	{
+		parent::boot();
+
+		static::created(function($answer){
+			$answer->question->increment('answers_count');
+			$answer->question->save();
+		});
+
 	}
 }
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -6,11 +6,11 @@ use App\Model;
 use Faker\Generator as Faker;
 
 $factory->define(App\Question::class, function (Faker $faker) {
-    return [
-        'title' => rtrim($faker->sentence(rand(5,10)), '.'),
-        'body' => $faker->paragraphs(rand(3,7), true),
-        'views' => rand(0,10),
-        'answers_count' => rand(0,10),
-        'votes' => rand(-10,10),
-    ];
+	return [
+		'title' => rtrim($faker->sentence(rand(5,10)), '.'),
+		'body' => $faker->paragraphs(rand(3,7), true),
+		'views' => rand(0,10),
+		// 'answers_count' => rand(0,10),
+		'votes' => rand(-10,10),
+	];
 });
